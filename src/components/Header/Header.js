4d62919488dbad9279af6d4f88b36c25422491e1
PCHIII <treyhelmer@gmail.com>
2019-10-15 13:54:58 -0400

    cleaned up code

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,15 +4,15 @@ import "./Header.css";
 const Header = props => (
     
       <div className="navbar navbar-expand-lg p-3 bg-primary text-white">
-        <div className="col-4 header-items nabbar-brand">
-          <button onClick={props.resetGame} className="reload-link btn btn-dark btn-lg">
+        <div className="col-4 header-items">
+          <button type="button" onClick={props.resetGame} className="reload-link btn btn-dark btn-lg">
            START GAME
         </button>
         </div>
         <div className="col-4 header-items text-center">
           <span>Click a Cheerleader to Score!!</span>
         </div>
-        <div className=" header-items nav-item ml-auto">
+        <div className=" header-items nav-item ml-auto mr-2">
           <span className="scores"> Your Score:  {props.currentScore}  <span className='divide pl-3 pr-3'>| </span> Top Score:  {props.topScore}
           </span>
         </div>
