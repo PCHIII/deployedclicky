725d55fca0bea0dee4acf819f9bed876b974a50c
PCHIII <treyhelmer@gmail.com>
2019-10-15 12:43:52 -0400

    text change jumbo

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -3,7 +3,7 @@ import "./Jumbotron.css";
 
 const Jumbotron = props => (
     <div className="jumbotron text-white bg-dark">
-        <h2>Click on a Girl to earn points, don't hit on the same one twice!</h2>
+        <h2>Click on a Girl to Score 1 Point,  don't hit on the same girl twice! (12 Points Wins!)</h2>
        
     </div>
 )
