c4b61d550f8a81b5692a72ad05292f07efd370ad
PCHIII <treyhelmer@gmail.com>
2019-10-16 15:25:33 -0400

    more starts

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -2,8 +2,8 @@ import React from "react";
 import "./Jumbotron.css";
 
 const Jumbotron = props => (
-    <div className="jumbotron text-white bg-dark"><span class="fa fa-star fa-lg pr-5"></span>
-        <span>Click on a Girl to Score 1 Point!  Don't hit on the same Girl twice! (12 Points Wins!)</span><span class="fa fa-star fa-lg pl-5"></span>
+    <div className="jumbotron text-white bg-dark"><span class="fa fa-star fa-lg pr-4 blue"></span><span class="fa fa-star fa-lg pr-4"></span><span class="fa fa-star fa-lg pr-4 blue"></span><span class="fa fa-star fa-lg pr-4"></span><span class="fa fa-star fa-lg pr-5 blue"></span>
+        <span>Click on a Girl to Score 1 Point!  Don't hit on the same Girl twice! (12 Points Wins!)</span><span class="fa fa-star fa-lg pl-5 blue"></span><span class="fa fa-star fa-lg pl-4"></span><span class="fa fa-star fa-lg pl-4 blue"></span><span class="fa fa-star fa-lg pl-4"></span><span class="fa fa-star fa-lg pl-4 blue"></span>
        
     </div>
 )
