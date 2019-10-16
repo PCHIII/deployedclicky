2889e83632d906b7db5600a39d136c83b77e5a41
PCHIII <treyhelmer@gmail.com>
2019-10-16 00:52:23 -0400

    fontawesome

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -2,8 +2,8 @@ import React from "react";
 import "./Jumbotron.css";
 
 const Jumbotron = props => (
-    <div className="jumbotron text-white bg-dark">
-        <h2>Click on a Girl to Score 1 Point!  Don't hit on the same Girl twice! (12 Points Wins!)</h2>
+    <div className="jumbotron text-white bg-dark"><span class="fa fa-star fa-lg pr-5"></span>
+        <span>Click on a Girl to Score 1 Point!  Don't hit on the same Girl twice! (12 Points Wins!)</span><span class="fa fa-star fa-lg pl-5"></span>
        
     </div>
 )
